b5dd8fd0435dfea47103a07c2adcea6414514c69
Gilthanas122 <pityu.django@gmail.com>
2019-01-29 12:11:24 +0100

    add credetials 1

--- main.tf
+++ main.tf
@@ -2,15 +2,14 @@ resource "aws_instance" "example" {
   ami = "ami-6d27a913"
   instance_type = "t2.micro"
 
+  connection {
+    user = "root"
+    type = "ssh"
+    public_key = "${file(AWS_ACCESS_KEY_ID)}"
+    private_key = "${file(AWS_SECRET_ACCESS_KEY)}"
+    timeout = "2m"
+  }
 }
 
-variable text{
-type = "string"
- default= "Hello pityu"
- }
-
-output "address" {
-  value = "${var.text}"
-}
 
 
